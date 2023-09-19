e301dc4f0fa0279aaa0a198cd42becaf1565459d
Ygor Carmo <ygor.carmo@gmail.com>
2023-09-20 09:32:00 +1000

    feat: single image stays in the center of slide and cached values that are being used in multiple places

--- src/PowerPointCreator.js
+++ src/PowerPointCreator.js
@@ -1,6 +1,24 @@
 import lookLogo from "./assets/Look_Hor_CMYK_300.jpg";
 import PptxGenJS from "pptxgenjs";
 
+const slideWidth = 10;
+
+const lookLogoProps = {
+  x: 7.5,
+  y: 0.2,
+  w: 2,
+  h: 0.25,
+  path: lookLogo,
+  objectName: "Look Logo",
+};
+
+const locationTitleProps = {
+  x: 1.13,
+  y: 0.1,
+  w: "50%",
+  h: "20%",
+};
+
 export default async function createPptx(images, title) {
   let pptx = new PptxGenJS();
 
@@ -14,32 +32,17 @@ export default async function createPptx(images, title) {
     align: "center",
   });
 
-  firstSlide.addImage({
-    x: 8.5,
-    y: 0.1,
-    w: 1.25,
-    h: 0.25,
-    path: lookLogo,
-    objectName: "animated gif",
-  });
+  firstSlide.addImage(lookLogoProps);
 
   let slide = pptx.addSlide();
-  slide.addImage({
-    x: 8.5,
-    y: 0.1,
-    w: 1.25,
-    h: 0.25,
-    path: lookLogo,
-    objectName: "animated gif",
-  });
+  slide.addImage(lookLogoProps);
 
   let initialXPosition = 1.13;
-  let textYPosition = 0.1;
   let imageYPosition = 2;
   let space = 3;
   let nextPosition = 0;
   let imageCount = 0;
-  let width = "20%";
+  let imageWidth = "20%";
   let height = "50%";
 
   let currentLocation = "";
@@ -54,48 +57,37 @@ export default async function createPptx(images, title) {
       nextLocation = nextFilename[1];
     }
 
-    if (currentLocation != nextLocation) {
-      slide.addText(currentFilename[0].concat("-", currentFilename[1]), {
-        x: initialXPosition,
-        y: textYPosition,
-        w: width,
-        h: height,
-      });
+    if (currentLocation != nextLocation && imageCount == 0) {
+      slide.addText(
+        currentFilename[0].concat("-", currentFilename[1]),
+        locationTitleProps
+      );
       slide.addImage({
-        x: initialXPosition,
-        y: imageYPosition,
-        w: width,
+        w: 4,
         h: height,
+        y: imageYPosition,
+        x: (slideWidth - 4) / 2,
         path: images[x].url,
-        objectName: "animated gif",
+        objectName: currentFilename[1],
       });
       imageCount = 0;
       initialXPosition = 1.13;
       nextPosition = 0;
       slide = pptx.addSlide();
-      slide.addImage({
-        x: 8.5,
-        y: 0.06,
-        w: 1.25,
-        h: 0.25,
-        path: lookLogo,
-        objectName: "animated gif",
-      });
+      slide.addImage(lookLogoProps);
     } else if (currentLocation == nextLocation && imageCount == 0) {
-      slide.addText(currentFilename[0].concat("-", currentFilename[1]), {
-        x: initialXPosition,
-        y: textYPosition,
-        w: width,
-        h: height,
-      });
+      slide.addText(
+        currentFilename[0].concat("-", currentFilename[1]),
+        locationTitleProps
+      );
 
       slide.addImage({
         x: initialXPosition,
         y: imageYPosition,
-        w: width,
+        w: imageWidth,
         h: height,
         path: images[x].url,
-        objectName: "animated gif",
+        objectName: currentFilename[1],
       });
       nextPosition = initialXPosition + space;
       imageCount++;
@@ -103,10 +95,10 @@ export default async function createPptx(images, title) {
       slide.addImage({
         x: nextPosition,
         y: imageYPosition,
-        w: width,
+        w: imageWidth,
         h: height,
         path: images[x].url,
-        objectName: "animated gif",
+        objectName: currentFilename[1],
       });
       nextPosition = nextPosition + space;
       imageCount++;
@@ -117,14 +109,7 @@ export default async function createPptx(images, title) {
       initialXPosition = 1.13;
       nextPosition = 0;
       slide = pptx.addSlide();
-      slide.addImage({
-        x: 8.5,
-        y: 0.06,
-        w: 1.25,
-        h: 0.25,
-        path: lookLogo,
-        objectName: "animated gif",
-      });
+      slide.addImage(lookLogoProps);
     }
   }
 
